476e4c342040f78c073e62983553f6aa6fdf5088
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-09-29 21:21:53 -0400

    recommit to save changes mentioned in previous commit.

--- database/updateStudentDetails.js
+++ database/updateStudentDetails.js
@@ -7,10 +7,6 @@ async function updateStudentDetails(studentID, firstname, lastname, email, id) {
       values = [firstname, lastname, email, id];
       await client.query(sqlText, values);
 
-      //   const sqlText2 = `INSERT INTO "students" (id, firstname, lastname, email) VALUES ($1, $2, $3, $4)`;
-      //   const values = [studentID, firstname, lastname, email];
-      //   await client.query(sqlText2, values);
-      console.log(studentID, firstname, lastname, email);
       if (err) {
         console.log(err);
       }
