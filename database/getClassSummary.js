3218f7582f7be2bcc31a242418bed76e79e47dc7
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-10-07 10:53:20 -0400

    Class summary data aggregation now returns array of objects which contains assignment name, assignment weight, and assignment average.

--- database/getClassSummary.js
+++ database/getClassSummary.js
@@ -5,26 +5,53 @@ const getClassSummary = async (classID) => {
   try {
     const sqlText = `SELECT * FROM "public".${classID} LEFT JOIN "public"."students" ON "public".${classID}."students" = "public"."students"."id"`;
     const classInfo = await pool.query(sqlText);
-    // console.log(classInfo.rows);
+    let assignmentMarks = {};
+    let assignments = [];
+    let assignmentAnalysis = [];
 
-    let marksData = classInfo.rows.map(async (entry) => {
-      let assignmentMarks = [];
-      if (entry.assignments) {
-        assignmentMarks[`${entry.assignments}`] = [];
-        await getAssignmentMarks(classID, entry.assignments).then((res) =>
-          res.forEach((datum) => {
+    let marksData = await Promise.all(
+      classInfo.rows.map(async (entry) => {
+        if (entry.assignments && !assignments.includes(entry.assignments)) {
+          assignments.push(entry.assignments);
+          assignmentMarks[`${entry.assignments}`] = { scores: [], outof: null };
+
+          let marks = await getAssignmentMarks(classID, entry.assignments);
+          marks.forEach((datum) => {
             if (datum.score) {
-              assignmentMarks[`${entry.assignments}`].push(datum.score);
+              {
+                assignmentMarks[`${entry.assignments}`].scores.push(
+                  datum.score
+                );
+              }
             }
-            // console.log(assignmentMarks[`${entry.assignments}`]);
-          })
-        );
-      }
-      return assignmentMarks;
+            if (datum.outof) {
+              assignmentMarks[`${entry.assignments}`].outof = datum.outof;
+            }
+          });
+        }
+        return assignmentMarks;
+      })
+    ).then((res) => {
+      return res;
     });
 
-    console.log(marksData);
-    return classInfo.rows;
+    // console.log(marksData[0][`${assignments[0]}`]);
+    for (let i = 0; i < assignments.length; i++) {
+      let assignmentTotal = marksData[i][`${assignments[i]}`].scores.reduce(
+        (prev, current) => parseInt(prev) + parseInt(current),
+        0
+      );
+      let assignmentAverage =
+        assignmentTotal / marksData[i][`${assignments[i]}`].scores.length;
+      let analysis = {
+        name: `${assignments[i]}`,
+        average: assignmentAverage,
+        outof: marksData[i][`${assignments[i]}`].outof,
+      };
+      assignmentAnalysis.push(analysis);
+    }
+
+    return { classInfo: classInfo.rows, marksData: assignmentAnalysis };
   } catch (err) {
     console.log(err);
   }
