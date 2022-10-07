18f1c42e024cd5288a580c04077f0c2067c290f7
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-10-06 20:40:00 -0400

    Class summary mark aggregating function implemented.

--- database/getClassSummary.js
+++ database/getClassSummary.js
@@ -1,9 +1,29 @@
 const { pool } = require("./database");
+const { getAssignmentMarks } = require("./getAssignmentMarks");
 
 const getClassSummary = async (classID) => {
   try {
     const sqlText = `SELECT * FROM "public".${classID} LEFT JOIN "public"."students" ON "public".${classID}."students" = "public"."students"."id"`;
     const classInfo = await pool.query(sqlText);
+    // console.log(classInfo.rows);
+
+    let marksData = classInfo.rows.map(async (entry) => {
+      let assignmentMarks = [];
+      if (entry.assignments) {
+        assignmentMarks[`${entry.assignments}`] = [];
+        await getAssignmentMarks(classID, entry.assignments).then((res) =>
+          res.forEach((datum) => {
+            if (datum.score) {
+              assignmentMarks[`${entry.assignments}`].push(datum.score);
+            }
+            // console.log(assignmentMarks[`${entry.assignments}`]);
+          })
+        );
+      }
+      return assignmentMarks;
+    });
+
+    console.log(marksData);
     return classInfo.rows;
   } catch (err) {
     console.log(err);
