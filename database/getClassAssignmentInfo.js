3e41f957d8b6e822577ce82496bc05d823709419
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-10-10 11:16:39 -0400

    Changed join type in assignments get function to only retrieve relevant results.

--- database/getClassAssignmentInfo.js
+++ database/getClassAssignmentInfo.js
@@ -2,7 +2,7 @@ const { pool } = require("./database");
 
 const getClassAssignmentInfo = async (classID) => {
   try {
-    const sqlText = `SELECT * FROM "public".${classID} FULL OUTER JOIN "public"."students" ON "public".${classID}."students" = "public"."students"."id"`;
+    const sqlText = `SELECT * FROM "public".${classID} LEFT JOIN "public"."students" ON "public".${classID}."students" = "public"."students"."id"`;
     const classInfo = await pool.query(sqlText);
     return classInfo.rows;
   } catch (err) {
