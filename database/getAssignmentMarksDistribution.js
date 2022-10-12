e997199b885169db0719b2e7d2ae01b12e8876f0
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-10-12 19:14:51 -0400

    getAssignmentMarksDistribution now delivers the data in the shape where the bargraph can use it.

--- database/getAssignmentMarksDistribution.js
+++ database/getAssignmentMarksDistribution.js
@@ -9,7 +9,67 @@ const getAssignmentMarksDistribution = async (classID, assignmentID) => {
 
     const sqlText2 = `SELECT * FROM "public".${assignmentTable} LEFT JOIN "public"."students" ON "public".${assignmentTable}."student" = "public"."students"."id" ORDER BY "lastname"`;
     const assignmentInfo = await pool.query(sqlText2);
-    return assignmentInfo.rows;
+
+    let outof;
+    let scores = [];
+    const distribution = [
+      { score: "NM", number: 0 },
+      { score: "<50%", number: 0 },
+      { score: "50-59%", number: 0 },
+      { score: "60-69%", number: 0 },
+      { score: "70-79%", number: 0 },
+      { score: "80-89%", number: 0 },
+      { score: "90-100%", number: 0 },
+    ];
+
+    assignmentInfo.rows.forEach((entry) => {
+      if (entry.outof) {
+        outof = parseInt(entry.outof);
+      }
+    });
+
+    // populates scores array with raw scores
+    assignmentInfo.rows.forEach((entry) => {
+      if (entry.score) {
+        scores.push(parseInt(entry.score));
+      }
+      if (entry.student && !entry.score) {
+        scores.push("NM");
+      }
+    });
+
+    // replaces raw score to its percentage
+    scores.forEach((score, index) => {
+      if (typeof score === "number") {
+        scores.splice(index, 1, score / outof);
+      }
+    });
+
+    scores.forEach((score) => {
+      if (score === "NM") {
+        distribution[0].number = distribution[0].number + 1;
+      }
+      if (score < 0.5) {
+        distribution[1].number = distribution[1].number + 1;
+      }
+      if (score >= 0.5 && score < 0.6) {
+        distribution[2].number = distribution[2].number + 1;
+      }
+      if (score >= 0.6 && score < 0.7) {
+        distribution[3].number = distribution[3].number + 1;
+      }
+      if (score >= 0.7 && score < 0.8) {
+        distribution[4].number = distribution[4].number + 1;
+      }
+      if (score >= 0.8 && score < 0.9) {
+        distribution[5].number = distribution[5].number + 1;
+      }
+      if (score >= 0.9) {
+        distribution[6].number = distribution[6].number + 1;
+      }
+    });
+
+    return distribution;
   } catch (err) {
     console.log(err);
   }
